b73cf71632dd58e02718d4fefc2aff5bdcdf668b
Arthur Matias <ahmmfdc@gmail.com>
2023-02-12 22:08:49 -0300

    fixing light

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import './style.css';
 import * as THREE from 'three';
-import tex from './assets/Moon Textures/Moon_Diff.png';
-import texDisplacement from './assets/Moon Textures/Moon_Disp.png';
+import tex from './assets/textures/Moon_Diff.png';
+import texDisplacement from './assets/textures/Moon_Disp.png';
 import SkyBox from './SkyBox';
 
 let scene: THREE.Scene
@@ -9,9 +9,8 @@ let renderer: THREE.WebGLRenderer;
 let camera: THREE.PerspectiveCamera;
 let moon: THREE.Mesh;
 
-let directionalLight: THREE.DirectionalLight
+// let ambientLight: THREE.AmbientLight
 
-let raycaster = new THREE.Raycaster();
 let mouse = new THREE.Vector2()
 
 let skyBox: SkyBox
@@ -38,29 +37,24 @@ function init(){
   
   // ---- CAMERA ----
 
-  camera.position.set(1200, -250, 100);
+  camera.position.set(600, -250, 100);
   camera.lookAt(new THREE.Vector3(0,0,0));
   
 
   // ---- LIGHTS ----
 
-  let ambientLight = new THREE.AmbientLight(0xcccccc, 0.2);
-  directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+  let ambientLight = new THREE.AmbientLight(0xffffff);
 
   // // ---- 3D CUBE ----
 
   const texture = new THREE.TextureLoader().load(tex);
-  const geometry = new THREE.SphereGeometry( 500, 300, 300);
-  const material = new THREE.MeshStandardMaterial({map: texture})
+  const geometry = new THREE.SphereGeometry( 300, 300, 300);
+  const material = new THREE.MeshPhongMaterial({map: texture})
   material.displacementMap = new THREE.TextureLoader().load(texDisplacement)
-  // const dispMat = new THREE.({map: texture})
-  
 
   moon = new THREE.Mesh(geometry, material);
   skyBox = new SkyBox();
-  // scene.add( ambientLight );
   scene.add( camera );
-  scene.add( directionalLight );
   scene.add( ambientLight );
   scene.add( skyBox.mesh );
   scene.add( moon );
@@ -68,15 +62,8 @@ function init(){
 }
 
 function render(){
-  // skyBox.mesh.rotation.x += 0.005;
-  // skyBox.mesh.rotation.y += 0.001;
-  // camera.rotateX(0.001)
-  directionalLight.position.x = mouse.x
-  // directionalLight.position
-  // directionalLight.position.y = mouse.y
-  // directionalLight.position.z = mouse.y
-  camera.position.set(1200 , -250, 200 * Math.cos(angle));
-  console.log(camera.position.sub(new THREE.Vector3(0,0,0)))
+  camera.position.set(600 , -250, 200 * Math.cos(angle));
+
   camera.lookAt(new THREE.Vector3(0,0,0));
   renderer.render( scene, camera )
   angle+=0.01
